a5ee9c9af57b8644fe7c445d27f4839a409beb86
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-08 00:41:36 +0530

    Initial commit

---
+++ Project - Hustle Mode App/Project - Hustle Mode App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project - Hustle Mode App
+//
+//  Created by Rudrank Riyam on 08/06/18.
+//  Copyright © 2018 iOS-Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
