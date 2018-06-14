8815c414fac66d7c357dc367c3299c7ab8e32a3b
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-14 07:00:57 +0530

    Update

--- Project - Swoosh App/Project - Swoosh App/Views/BorderButton.swift
+++ Project - Swoosh App/Project - Swoosh App/Views/BorderButton.swift
@@ -10,12 +10,9 @@ import UIKit
 
 class BorderButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
     }
-    */
-
 }
