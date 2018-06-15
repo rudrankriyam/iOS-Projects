26ec903b4be8db39557c88f734f0ca0069af13f9
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-15 08:46:15 +0530

    Initial commit

---
+++ Project - Mini Music Player App/Project - Mini Music Player App/GenreButtonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GenreButtonViewController.swift
+//  Project - Mini Music Player App
+//
+//  Created by Rudrank Riyam on 15/06/18.
+//  Copyright © 2018 iOS-Projects. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
