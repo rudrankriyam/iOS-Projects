62ea5c9dc90c7bdc5d8924829d3c5064b9b7dbaa
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-17 09:46:20 +0530

    AVAudioPlayer

--- Project - Xylophone App/Xylophone/ VIewController.swift
+++ Project - Xylophone App/Xylophone/ VIewController.swift
@@ -9,22 +9,29 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
+    var audioPlayer : AVAudioPlayer!
+    var selectedSoundFileName : String = "'"
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
-        
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        playsound()
     }
     
-  
-
+    func playsound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
 }
-
