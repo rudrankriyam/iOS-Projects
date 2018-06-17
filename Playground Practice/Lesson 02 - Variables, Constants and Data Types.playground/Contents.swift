3f0b9450df60d49bf56c9ab1469cf63091139cc9
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 05:23:31 +0530

    Update Lesson 2 - Variables, Constants and Data Types

--- Playground Practice/Lesson 02 - Variables, Constants and Data Types.playground/Contents.swift
+++ Playground Practice/Lesson 02 - Variables, Constants and Data Types.playground/Contents.swift
@@ -5,3 +5,16 @@
 
 import UIKit
 
+var myAge : Int = 19
+myAge += 1
+
+let myFirstName : String = "Rudrank"
+let myAgeInTenYears = myAge + 10
+let myFullName = "\(myFirstName) Riyam"
+let myDetails = "\(myFullName), \(myAge)"
+
+let wholeNumbers : Int = 12
+let text : String = "abc"
+let booleans : Bool = true
+let floatingPointNumber : Float = 1.3
+let double : Double = 3.14159265359
