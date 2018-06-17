3edf796a656e7948a65615f4bee636c73cbe3f1b
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 05:04:47 +0530

    Add Lesson 1 Comments, Print and Console

---
+++ Playground Practice/Lesson 01 - Comments, Print and Console.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+// Lesson 1 - Comments, Print and Console
+// iOS 11 & Swit 4 - The Complete iOS App Development Bootcamp
+
+import UIKit
+
+/* for writing a lot of
+ comments
+ */
+
+var str = "Hello, playground"
