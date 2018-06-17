589c6f87c8c2930704010d07a3792342dcf8d12a
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-17 11:36:50 +0530

    Initial commit

---
+++ Project - Quiz App/Quizzler/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Project - Quizzler App
+//
+//  Created by Rudrank Riyam on 17/06/18.
+//  Copyright © 2018 iOS-Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
