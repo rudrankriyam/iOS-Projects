acbf0919d178b2b9183a6c8da967f67101bac20d
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 05:13:06 +0530

    Add Lesson 2 - Variables, Constants and Data Types

---
+++ Playground Practice/Lesson 02 - Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+// Lesson 2 - Variables, Constants and Data Types
+// iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
+
+import UIKit
+
