5f95f1e1054606aa6d8c62c44ac139cffd552521
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 06:39:06 +0530

    Update Lesson 4 - Conditional Statements

--- Playground Practice/Lesson 04 - Conditional Statements.playground/Contents.swift
+++ Playground Practice/Lesson 04 - Conditional Statements.playground/Contents.swift
@@ -8,10 +8,12 @@ func loveCalculator(yourName : String, theirName : String) -> String {
     let loveScore = arc4random_uniform(101)
     
     if loveScore > 80 {
-        return "You love each other like Kany love Kanye"
+        return "Your love score is \(loveScore). You love each other like Kany love Kanye"
+    } else if loveScore > 40 && loveScore <= 80 {
+        return "Your love score is \(loveScore). You go together like coke and mentos"
     } else {
-        return "No love possible, you will be forever alone!"
+        return "Your love score is \(loveScore). No love possible, you will be forever alone!"
     }
 }
 
-loveCalculator(yourName: "Rudrank", theirName: "Swift")
+print(loveCalculator(yourName: "Rudrank", theirName: "Swift"))
