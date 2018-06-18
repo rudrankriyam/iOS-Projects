25418c86aaef5c385be4fa0488e61e387f862834
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 05:43:35 +0530

    Update Lesson 3 - Functions

--- Playground Practice/Lesson 03 - Functions.playground/Contents.swift
+++ Playground Practice/Lesson 03 - Functions.playground/Contents.swift
@@ -3,6 +3,9 @@
 // Lesson 3 - Functions
 // iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
 
-func nameOfFunction() {
-    
+func getMilk() {
+    print("Go to the shops")
+    print("Buy 2 Cartons of milk")
+    print("Pay $2")
+    print("Come Home")
 }
