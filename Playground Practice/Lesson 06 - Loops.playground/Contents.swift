beb66f7bfc4924951abfa29a0705653a41c3bdf0
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 08:26:16 +0530

    Add Lesson 6 - Loops

---
+++ Playground Practice/Lesson 06 - Loops.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Lesson 6 - Loops
+// iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
+
+let arrayOfNumbers = [1,5,3,6,2,7,23,34]
+
+for number in arrayOfNumbers {
+    print(number)
+}
