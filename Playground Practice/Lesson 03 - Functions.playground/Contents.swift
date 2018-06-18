6258803d6fd6e7567ea5c1c9efc11672f2733d88
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 06:14:00 +0530

    Update V3 Lesson 3 - Functions

--- Playground Practice/Lesson 03 - Functions.playground/Contents.swift
+++ Playground Practice/Lesson 03 - Functions.playground/Contents.swift
@@ -3,7 +3,14 @@
 // Lesson 3 - Functions
 // iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
 
-func getMilk(howManyMilkCartons : Int) {
+func getMilkPartOne() {
+    print("Go to the shops")
+    print("Buy 2 Cartons of milk")
+    print("Pay $2")
+    print("Come Home")
+}
+    
+func getMilkPartTwo(howManyMilkCartons : Int) {
     
     let priceToPay = howManyMilkCartons * 2
 
@@ -11,7 +18,25 @@ func getMilk(howManyMilkCartons : Int) {
     print("Buy \(howManyMilkCartons) Cartons of milk")
     print("Pay $\(priceToPay)")
     print("Come Home")
-
 }
 
-getMilk(howManyMilkCartons: 4)
+func getMilkPartThree(howManyMilkCartons : Int, howMuchMoneyRobotWasGiven : Int) -> Int {
+
+    let priceToPay = howManyMilkCartons * 2
+    let change = howMuchMoneyRobotWasGiven - priceToPay
+    
+    print("Go to the shop")
+    print("Buy \(howManyMilkCartons) Cartons of milk")
+    print("Pay $\(priceToPay)")
+    print("Come Home")
+
+    return change
+}
+
+getMilkPartOne()
+getMilkPartTwo(howManyMilkCartons: 4)
+
+// Calling the getMilkPartThree() Function
+var amountOfChange = getMilkPartThree(howManyMilkCartons: 4, howMuchMoneyRobotWasGiven: 10)
+
+print("Hello Master, here is your $\(amountOfChange) change")
