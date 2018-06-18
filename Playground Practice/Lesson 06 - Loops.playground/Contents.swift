2ba842dc872880bd1c80256cca237b3b5003ace7
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 09:42:14 +0530

    Update Lesson 6 - Loops

--- Playground Practice/Lesson 06 - Loops.playground/Contents.swift
+++ Playground Practice/Lesson 06 - Loops.playground/Contents.swift
@@ -3,8 +3,20 @@ import UIKit
 // Lesson 6 - Loops
 // iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
 
-let arrayOfNumbers = [1,5,3,6,2,7,23,34]
+var sum = 0
+
+let arrayOfNumbers = [1, 5, 3, 6, 2, 7, 23, 34, 21, 12]
 
 for number in arrayOfNumbers {
+    sum += number
+}
+
+print(sum)
+
+for number in 1...10 {
+    print(number)
+}
+
+for number in 1..<10 where number % 2 == 0 {
     print(number)
 }
