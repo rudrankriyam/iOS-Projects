37b80be04f34eed6098ef44d96d93417db7858cd
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 05:53:43 +0530

    Update V2 Lesson 3 - Functions

--- Playground Practice/Lesson 03 - Functions.playground/Contents.swift
+++ Playground Practice/Lesson 03 - Functions.playground/Contents.swift
@@ -3,9 +3,15 @@
 // Lesson 3 - Functions
 // iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
 
-func getMilk() {
-    print("Go to the shops")
-    print("Buy 2 Cartons of milk")
-    print("Pay $2")
+func getMilk(howManyMilkCartons : Int) {
+    
+    let priceToPay = howManyMilkCartons * 2
+
+    print("Go to the shop")
+    print("Buy \(howManyMilkCartons) Cartons of milk")
+    print("Pay $\(priceToPay)")
     print("Come Home")
+
 }
+
+getMilk(howManyMilkCartons: 4)
