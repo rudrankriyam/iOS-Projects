72e45e0a1692755766193e736aa39558a2b41a8c
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 16:05:59 +0530

    Update Lesson 7 - Classes and Objects

--- Playground Practice/Lesson 07 - Classes and Objects.playground/Contents.swift
+++ Playground Practice/Lesson 07 - Classes and Objects.playground/Contents.swift
@@ -3,6 +3,24 @@ import UIKit
 // Lesson 7 - Classes and Objects
 // iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
 
-class Car {
-    
+enum CarType {
+    case Sedan
+    case Coupe
+    case Hatchback
 }
+
+class Car {
+    var color : String = "Black"
+    var numberOfSeats : Int = 5
+    var typeOfCar : CarType = .Coupe
+    
+    init(customerChosenColor : String) {
+        color = customerChosenColor
+    }
+}
+
+let myCar = Car(customerChosenColor: "Red")
+
+print(myCar.color)
+print(myCar.numberOfSeats)
+print(myCar.typeOfCar)
