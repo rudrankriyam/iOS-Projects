d65d4ec3d189cceb93946046804726005aa5a693
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-18 07:11:23 +0530

    Update Lesson 5 - BMI Calculator Challenge

--- Playground Practice/Lesson 05 - BMI Calculator Challenge .playground/Contents.swift
+++ Playground Practice/Lesson 05 - BMI Calculator Challenge .playground/Contents.swift
@@ -1,5 +1,40 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+// Lesson 5 - BMI Calculator Challenge
+// iOS 11 & Swift 4 - The Complete iOS App Development Bootcamp
+
+func calcBMI(weight : Float, height : Float) -> String {
+    
+    // Mass in kgs and Height in metres
+    let bodyMassIndex = weight / pow(height, 2)
+    //For a BMI which is rounded off to one decimal places.
+    let shortenedBodyMassIndex = String(format: "%.1f", bodyMassIndex)
+    var interpretation = ""
+    
+    if bodyMassIndex > 30 {
+        interpretation = "You are Obese."
+    } else if bodyMassIndex > 25 {
+        interpretation = "You are Overweight."
+    } else if bodyMassIndex > 18.5 {
+        interpretation = "You are Normal."
+    } else {
+        interpretation = "You are underweight."
+    }
+    
+    return "Your BMI is \(shortenedBodyMassIndex). \(interpretation)"
+}
+
+func bmiCalcImperialUnits(weightInPounds : Float, heightInFeet : Float, remainderInches : Float ) -> String {
+    
+    let weightInKg = weightInPounds * 0.4535
+    let totalInches = (heightInFeet * 12) + remainderInches
+    let heightInM = totalInches * 0.0254
+    
+    return calcBMI(weight: weightInKg, height: heightInM)
+}
+
+let caseOne = calcBMI(weight: 102, height: 1.79)
+let caseTwo = bmiCalcImperialUnits(weightInPounds: 225, heightInFeet: 5, remainderInches: 10.5)
+
+print(caseOne)
+print(caseTwo)
