8d7c942ddde32e83fdfac12cdde284b1d21afbdc
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-19 13:52:07 +0530

    Add Enumeration.playground

---
+++ Swift Language /02 Language Guide/08 Enumerations/Enumerations.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Enumerations
+
+import UIKit
+
+// Enumeration Syntax
+
+enum SomeEnumerations {
+    // enumeration definition goes here
+}
+
+enum CompassPoint {
+    case north
+    case south
+    case west
+    case east
+}
+
+enum Planet {
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+var directionToHead =
+
