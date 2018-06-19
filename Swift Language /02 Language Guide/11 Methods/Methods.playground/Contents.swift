a233e3fed0a73714320eb3f02098946bf810388e
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-19 06:54:40 +0530

    Add Methods.playground

---
+++ Swift Language /02 Language Guide/11 Methods/Methods.playground/Contents.swift
@@ -0,0 +1,47 @@
+// Methods
+
+import UIKit
+
+// Instance Methods
+
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+    }
+    func increment(by amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+let counter = Counter()
+// the initial counter value is 0
+counter.increment()
+// the counter's value is now 1
+counter.increment(by: 5)
+// the counter's value is now 6
+counter.reset()
+// the counter's value is now 0
+
+// The self property
+
+func incrementSecond() {
+    // self.counting += 1
+}
+
+struct Point {
+    var x = 0.0, y = 0.0
+    func isToTheRightOf(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+let somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOf(x: 1.0) {
+    print("This point is to the right of the line where x == 1.0")
+}
+
+
+
