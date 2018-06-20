d982c17c1b9f47a9ba35945cad1a882a23b5bc46
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-20 08:41:17 +0530

    Update Enumeration.playground

--- Swift Language /02 Language Guide/08 Enumerations/Enumerations.playground/Contents.swift
+++ Swift Language /02 Language Guide/08 Enumerations/Enumerations.playground/Contents.swift
@@ -19,5 +19,22 @@ enum Planet {
     case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
 }
 
-var directionToHead =
+var directionToHead = CompassPoint.west
+
+directionToHead = .east
+
+// Matching Enumeration Values with a Switch Statement
+
+directionToHead = .south
+
+switch directionToHead {
+case .north:
+    print("Lots of planet have a north")
+case .south:
+    print("Watch out of penguins")
+case .east:
+    print("Where the sun rises")
+case .west:
+    print("Where the skies are blue")
+}
 
