c662493357218b3bb74d9d77b5d5d275fbe8c3e5
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-21 07:42:48 +0530

    Update ASwiftTour.playground

--- Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
+++ Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
@@ -28,8 +28,8 @@ I said "I have \(apples) apples."
 And then I said "I have \(apples + oranges) pieces of fruit."
 """
 
-var shoppingLIst = ["catfish", "water", "tulips", "bblue paint"]
-shoppingLIst[1] = "bottle of water"
+var shoppingList = ["catfish", "water", "tulips", "bblue paint"]
+shoppingList[1] = "bottle of water"
 
 var occupations = [
     "Malcolm": "Captain",
@@ -43,5 +43,75 @@ let emptyDictionary = [String: Float]()
 shoppingList = []
 occupations = [:]
 
+// Control Flow
 
+let individualScore = [75, 42, 103, 87, 12]
+var teamScore = 0
+for score in individualScore {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
 
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "hello, \(name)"
+}
+
+let nickName: String? = nil
+let fullName: String = "Rudy Rudyy"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log")
+case "cucumber", "watercress":
+    print("that would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
+
+// Functions and Closures
