6a73d4462cee3e76a2c553d6a9824e58ae3b151a
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-21 16:31:15 +0530

    Update V7 ASwiftTour.playground

--- Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
+++ Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
@@ -454,3 +454,35 @@ fridgeContains("banana")
 print(fridgeIsOpen)
 
 // Generics
+
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+makeArray(repeating: "knock", numberOfTimes: 4)
+
+// Reimplement the Swift standard library's optional type
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Iterator.Element: Equatable, T.Iterator.Element == U.Iterator.Element {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+}
+anyCommonElements([1, 2, 3], [3])
+
+// Writing <T: Equatable> is the same as writing <T> ... where T: Equatable.
