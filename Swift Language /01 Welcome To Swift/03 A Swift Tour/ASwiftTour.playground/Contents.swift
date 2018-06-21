75b18f94bd6b8ea54ea3d0285ac1f6d91fffd620
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-21 16:09:28 +0530

    Update V6 ASwiftTour.playground

--- Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
+++ Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
@@ -404,4 +404,53 @@ print(protocolValue.simpleDescription)
 
 // Error Handling
 
+enum PrinterError: Error {
+    case outPaper
+    case noToner
+    case onFire
+}
 
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+do {
+    let printerResponse = try send(job: 1040, toPrinter: "Bi Sheng")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+    let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
+
+let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
+
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+    }
+    
+    let result = fridgeContent.contains(food)
+    return result
+}
+fridgeContains("banana")
+print(fridgeIsOpen)
+
+// Generics
