4d186b062c4e8414f3ef6a68b49ccad89f790c70
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-21 12:43:04 +0530

    Update V5 ASwiftTour.playground

--- Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
+++ Swift Language /01 Welcome To Swift/03 A Swift Tour/ASwiftTour.playground/Contents.swift
@@ -363,4 +363,45 @@ let threeOfSpadesDescription = threeOfSpades.simpleDescription()
 
 // Protocols and Extension
 
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+print(7.simpleDescription)
+
+let protocolValue: ExampleProtocol = a
+print(protocolValue.simpleDescription)
+
+// Error Handling
+
 
