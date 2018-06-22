40485fe2dbbe10e3baa1df8f5b75e2403aa16b5d
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-22 05:55:41 +0530

    Update V2 TheBasics.playground

--- Swift Language /02 Language Guide/01 The Basics/TheBasics.playground/Contents.swift
+++ Swift Language /02 Language Guide/01 The Basics/TheBasics.playground/Contents.swift
@@ -62,7 +62,7 @@ let maxValue = UInt8.max // maxValue is equal to 255, and is of type UInt 8
 // Type Safety and Type Inference
 
 let meaningOfLife = 42 // Inferred to be of type Int
-let pi = 3.14159 // Inferred to be of type Double
+var pi = 3.14159 // Inferred to be of type Double
 let anotherPi = 3 + 0.14159 // Also inferred to be of type Double
 
 // Numeric Literal
@@ -84,7 +84,71 @@ let justOverOneMillion = 1_000_000.000_000_1
 
 // Integer Conversion
 
-let cannotBeNegative: UInt8 = -1 // UInt 8 cannot store negative numbers, and so this will report an error
-let tooBig: Int8 = Int8.max + 1 // Int8 cannot store a number larger than its maximum value
+// let cannotBeNegative: UInt8 = -1 // UInt 8 cannot store negative numbers, and so this will report an error
+// let tooBig: Int8 = Int8.max + 1 // Int8 cannot store a number larger than its maximum value
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+// Integer and Floating-Point Conversion
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+pi = Double(three) + pointOneFourOneFiveNine
+
+let integerPi = Int(pi)
+
+// Type Aliases
+
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+// maxAmplitudeFound is now 0
+
+// Booleans
+
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty turnips!")
+} else {
+    print("Eww, turnips are horrible.")
+}
+
+let i = 1
+
+if i == 1 {
+    // this example will compile successfully
+}
+
+// Tupeles
+
+let http404Error = (404, "Not Found")
+// http404Error is of type (int, String) and equals (404, "Not Found")
+
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+// "Prints The status code is 404"
+print("The status message is \(statusMessage)")
+// Prints "The status message is Not Found"
+
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+// "Prints The status code is 404"
+
+print("The status code is \(http404Error.0)")
+// "Prints The status code is 404"
+print("The status message is \(http404Error.1)")
+// Prints "The status message is Not Found"
+
+let http200Status = (statusCode: 200, description: "OK")
+
+print("The status code is \(http200Status.statusCode)")
+// "Prints The status code is 200"
+print("The status code is \(http200Status.description)")
+// Prints "The status message is OK"
+
+// Optionals
 
 
