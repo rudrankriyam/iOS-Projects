916a7c3f6c01f686e64543fae6dad990ee3e5fb0
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-26 11:31:57 +0530

    Add ErrorHandling.playground

---
+++ Swift Language /02 Language Guide/17 Error Handling/ErrorHandling.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Error Handling
+
+import UIKit
+
+// Representing and Throwing Errors
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+// Handling Errors
