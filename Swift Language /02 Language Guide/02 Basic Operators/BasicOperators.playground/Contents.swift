f9285d672ea8589168769d70dff4be3b65a21726
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-26 11:31:39 +0530

    Add BasicOperators.playground

---
+++ Swift Language /02 Language Guide/02 Basic Operators/BasicOperators.playground/Contents.swift
@@ -0,0 +1,31 @@
+// Basic Operators
+
+import UIKit
+
+// Terminology
+
+// Assignment Operator
+
+let b = 10
+var a = 5
+a = 5
+// a is now equal to 10
+
+let (x,y) = (1,2)
+// x is equal to 1, and y is equal to 2
+
+// Arithmetric Operators
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+"Hello, " + "World!"
+
+// Remainder Operator
+
+9 % 4 // equals 1
+-9 % 4 // equals -1
+
+
