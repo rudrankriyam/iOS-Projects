7747340fe2195b54c08c827c5c472bd667fceebd
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-27 14:02:42 +0530

    Update ErrorHandling.playground

--- Swift Language /02 Language Guide/17 Error Handling/ErrorHandling.playground/Contents.swift
+++ Swift Language /02 Language Guide/17 Error Handling/ErrorHandling.playground/Contents.swift
@@ -13,3 +13,45 @@ enum VendingMachineError: Error {
 throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
 
 // Handling Errors
+
+// Propagating Errors Using Throwing Functions
+
+// func canThrowErrors() throws -> String
+
+// func cannnotThrowErrors() -> String
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinsDeposited = 0
+    
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        
+        guard  item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+        coinsDeposited -= item.price
+        
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        
+        print("Dispension \(name)")
+    }
+}
+
