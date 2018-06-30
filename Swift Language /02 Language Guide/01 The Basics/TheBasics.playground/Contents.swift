de0155ff79adfdc08d51c8952a6974ec1cd488e2
Rudrank Riyam <rudrankriyam@gmail.com>
2018-06-30 14:44:22 +0530

    Update V5 TheBasics.playground

--- Swift Language /02 Language Guide/01 The Basics/TheBasics.playground/Contents.swift
+++ Swift Language /02 Language Guide/01 The Basics/TheBasics.playground/Contents.swift
@@ -259,4 +259,30 @@ do {
 
 // Assertions and Preconditions
 
+// assert(_:_:file:line:)
+
+// Debugging with Assertions
+
+let age = -3
+
+assert(age >= 0, "A person's age can't be less than zero.")
+// This assertion fails because -3 is not >=0
+
+// assertionFailure(_:file:line:)
+
+if age > 10 {
+    print("You can ride the roller-coaster or the ferris wheel.")
+} else if age > 0 {
+    print("You can ride the ferris wheel.")
+} else {
+    assertionFailure("A person's age can't be less than zero.")
+}
+
+// Enforcing Preconditions
+
+// In the implementation of a subscript...
+
+let index = -1
+precondition(index > 0, "Index must be greater than zero.")
+
 
