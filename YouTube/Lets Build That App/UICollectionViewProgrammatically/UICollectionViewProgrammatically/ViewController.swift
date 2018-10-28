3933f745bebb650da47278c401157d6f353dddcf
Rudrank Riyam <rudrankriyam@gmail.com>
2018-10-29 00:57:40 +0530

    Update V2 UICollectionViewProgrammatically

--- YouTube/Lets Build That App/UICollectionViewProgrammatically/UICollectionViewProgrammatically/ViewController.swift
+++ YouTube/Lets Build That App/UICollectionViewProgrammatically/UICollectionViewProgrammatically/ViewController.swift
@@ -8,30 +8,55 @@
 
 import UIKit
 
-class CustomCollectionViewController: UICollectionViewController {
+class CustomCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let customCellIdentifier = "customCellIdentifier"
+    let names = ["John D. Rockefeller", "Andrew Carnegie", "Thomas Edison", "Henry Ford", "Charles Merrill", "Sam Walton", "Charles Schwab", "Walt Disney", "Bill Gates", "Steve Jobs"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = UIColor.darkGray
+        collectionView?.backgroundColor = UIColor.white
         collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: customCellIdentifier)
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-       let customCell =  collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath)
-        
+       let customCell =  collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath) as! CustomCell
+        customCell.nameLabel.text = names[indexPath.item]
         return customCell
     }
     
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return names.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
     }
 }
 
 class CustomCell: UICollectionViewCell {
-        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Custom Text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        backgroundColor = UIColor.darkGray
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
-
-
