c8139bdb54e9dee41297b89eb3088c8faddc8f11
Rudrank Riyam <rudrankriyam@gmail.com>
2018-10-29 12:59:46 +0530

    Update CarouselUIPageViewController

--- YouTube/Lets Build That App/CarouselUIPageViewController/CarouselUIPageViewController/ViewController.swift
+++ YouTube/Lets Build That App/CarouselUIPageViewController/CarouselUIPageViewController/ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = UIColor.black
+        iv.image = UIImage(named: "puppy1")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.darkGray
+        view.addSubview(imageView)
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": imageView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": imageView]))
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
