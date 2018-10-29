5232bd82476260c7a2e568b3b9ea5845885336de
Rudrank Riyam <rudrankriyam@gmail.com>
2018-10-29 21:43:43 +0530

    Add ProjectorPageViewController

---
+++ YouTube/Lets Build That App/CarouselUIPageViewController/CarouselUIPageViewController/ProjectorPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProjectorPageViewController.swift
+//  CarouselUIPageViewController
+//
+//  Created by Rudrank Riyam on 29/10/18.
+//  Copyright © 2018 iOS-Projects. All rights reserved.
+//
+
+import UIKit
+
+class ProjectorPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
