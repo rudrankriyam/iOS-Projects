ace5bde8d82a32a95a15349c5c6b78e91cb4ec8f
Rudrank Riyam <rudrankriyam@gmail.com>
2018-10-30 23:54:20 +0530

    Add AppLikePinterest

---
+++ YouTube/Gary Tokman/AppLikePinterest/AppLikePinterest/Views/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  AppLikePinterest
+//
+//  Created by Rudrank Riyam on 30/10/18.
+//  Copyright © 2018 iOS-Projects. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
