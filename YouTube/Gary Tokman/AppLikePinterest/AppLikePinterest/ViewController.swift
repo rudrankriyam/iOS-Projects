dfc790c7b94f225e845d23d57eeffe1450eebe36
Rudrank Riyam <rudrankriyam@gmail.com>
2018-10-30 23:54:39 +0530

    Update AppLikePinterest

--- YouTube/Gary Tokman/AppLikePinterest/AppLikePinterest/ViewController.swift
+++ YouTube/Gary Tokman/AppLikePinterest/AppLikePinterest/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+}
+
+// MARK: Data Source
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell
+    }
 }
 
