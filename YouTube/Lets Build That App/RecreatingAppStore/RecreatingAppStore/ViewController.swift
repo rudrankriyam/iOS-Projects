3b42afe48ee709f41f2f1471c2e9c855297c8b3e
Rudrank Riyam <rudrankriyam@gmail.com>
2018-10-30 23:25:25 +0530

    Update RecreatingAppStore

--- YouTube/Lets Build That App/RecreatingAppStore/RecreatingAppStore/ViewController.swift
+++ YouTube/Lets Build That App/RecreatingAppStore/RecreatingAppStore/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeaturedAppsViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.red
         // Do any additional setup after loading the view, typically from a nib.
     }
 
