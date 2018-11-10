4bc18fa4b934755f66eaac969a2654a876672599
Rudrank Riyam <rudrankriyam@gmail.com>
2018-11-11 00:03:29 +0530

    Add CollectionViewProgrammatically

---
+++ YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewProgrammatically
+//
+//  Created by Rudrank Riyam on 11/11/18.
+//  Copyright © 2018 iOS-Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
