62bdc18fd5c276da8750e6d94a591cb89c367352
Rudrank Riyam <rudrankriyam@gmail.com>
2018-11-11 00:40:12 +0530

    Update V2 CollectionViewProgrammatically

--- YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/AppDelegate.swift
+++ YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let layout = UICollectionViewFlowLayout()
+        let mainViewController = ViewController(collectionViewLayout: layout)
+        window?.rootViewController = UINavigationController(rootViewController: mainViewController)
         return true
     }
 
--- YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/ViewController.swift
+++ YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/ViewController.swift
@@ -8,13 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.backgroundColor = UIColor.init(red: 44/255, green: 57/255, blue: 95/255, alpha: 1)
+        
+        navigationItem.title = "Teams"
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)]
+        
+        collectionView.register(TeamCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TeamCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width / 2) - 16, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+}
 
-
+class TeamCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let teamImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .green
+        return imageView
+    }()
+    
+    let teamNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    func setup() {
+        self.backgroundColor = UIColor.green
+    }
 }
 
