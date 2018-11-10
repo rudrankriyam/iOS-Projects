1cd885e06acc8ff78334a8a51447759fd6c85251
Rudrank Riyam <rudrankriyam@gmail.com>
2018-11-11 02:17:08 +0530

    Update V5 CollectionViewProgrammatically

--- YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/Extensions.swift
+++ YouTube/Pavel Bogart/CollectionViewProgrammatically/CollectionViewProgrammatically/Extensions.swift
@@ -79,38 +79,4 @@ extension UIView {
         }
         return rightAnchor
     }
-    
 }
-
-public extension UIColor {
-    
-    public convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
-        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
-    }
-    
-    static var customLightBlueColor: UIColor = {
-        return UIColor(r: 44, g: 57, b: 95)
-    }()
-    
-    static var customRedColor: UIColor = {
-        return UIColor(r: 217, g: 48, b: 80)
-    }()
-    
-    static var customDarkBlueColor: UIColor = {
-        return UIColor(r: 11, g: 22, b: 53)
-    }()
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
